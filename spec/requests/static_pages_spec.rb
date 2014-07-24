e2771d0ac2fe817a49c56cf0df28599d96c11ca5
KITE <ixkaito@gmail.com>
2014-07-25 06:22:31 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,12 @@ describe "Static pages" do
 
 		it "should have the content 'Sample Application'" do
 			visit '/static_pages/home'
-			expect(page).to have_content('Sample Application')
+			expect(page).to have_content('Home')
+		end
+
+		it "should have the title 'Sample Application'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Sample Application")
 		end
 	end
 
@@ -16,6 +21,11 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Help | Ruby on Rails Sample Application")
+		end
 	end
 
 	describe "About page" do
@@ -23,6 +33,11 @@ describe "Static pages" do
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("About Us | Ruby on Rails Sample Application")
 		end
 	end
 end
